f7a6d6602f2c26e50b81a89e96268ca40c2d7762
OlegKhudoerko <292oleg292@mail.ru>
2022-08-23 20:32:28 +0300

    decision34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -2,3 +2,15 @@
 //Напишите программу, которая покажет количество чётных чисел в массиве.
 
 // [345, 897, 568, 234] -> 2
+
+int[] array = new int[10];
+int size = array.Length;
+int count = 0;
+
+for (int i = 0; i < size; i++) 
+{
+array[i] = new Random().Next(100,1000);
+if (array[i]%2 == 0)    count++;
+}
+Console.WriteLine($"[{string.Join(", ", array)}] -> {count}");
+
